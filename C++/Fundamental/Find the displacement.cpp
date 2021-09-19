6484ee810b5604cb16de37507f9d2b87384d8ed8
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2021-09-19 19:55:37 +0530

    adding reverse version

--- C++/Fundamental/Find the displacement.cpp
+++ C++/Fundamental/Find the displacement.cpp
@@ -23,3 +23,27 @@ int main(){
 
     cout << "Displacement of (X,Y) is " << "(" <<x << "," <<y <<")";
 }
+
+	// For East and West
+	if(x>=0){
+		for(i=1;i<=x;i++){
+			cout<<"E";
+		}
+	}
+	else{
+		for(i=-1;i>=x;i--){
+			cout<<"W";
+		}
+	}
+	if(y>=0){
+		for(i=1;i<=y;i++){
+			cout<<"N";
+		}
+	}
+	else{
+		for(i=-1;i>=y;i--){
+			cout<<"S";
+		}
+	}
+	cout<<endl;
+}
